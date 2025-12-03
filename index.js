adee7de6469179286a2adc27a89188fd208ff9a3
rishikareddyvootkur <rrvootkur@gmail.com>
2025-12-02 23:54:46 -0500

    fixed 404

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
 import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js";
-import QuizRoutes from "./Kambaz/Quizzes/routes.js"; // NEW
-import QuizAttemptRoutes from "./Kambaz/QuizAttempts/routes.js"; // NEW
+import QuizRoutes from "./Kambaz/Quizzes/routes.js";
+import QuizAttemptRoutes from "./Kambaz/QuizAttempts/routes.js"; 
 import "dotenv/config";
 import session from "express-session";
 import Lab5 from "./Lab5/index.js";
